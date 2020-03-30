2ce1632ba1c30f9b5b6a6e7780d21149f31b69c0
ziolokeven <ziolokeven@gmail.com>
2020-03-30 12:46:35 +0200

    add zadanie2

--- complex.js
+++ complex.js
@@ -1,28 +1,28 @@
-var Complex = /** @class */ (function () {
-    function Complex(rzeczywista, urojona) {
+"use strict";
+class Complex {
+    constructor(rzeczywista, urojona) {
         this.rzeczywista = rzeczywista;
         this.urojona = urojona;
     }
-    Complex.prototype.dodawanie = function (obj) {
+    dodawanie(obj) {
         return new Complex(this.rzeczywista + obj.rzeczywista, this.urojona + obj.urojona);
-    };
-    Complex.prototype.odejmowanie = function (obj) {
+    }
+    odejmowanie(obj) {
         return new Complex(this.rzeczywista - obj.rzeczywista, this.urojona - obj.urojona);
-    };
-    Complex.prototype.modul = function () {
-        var rzeczywista = Math.pow(this.rzeczywista, 2);
-        var urojona = Math.pow(this.urojona, 2);
+    }
+    modul() {
+        let rzeczywista = Math.pow(this.rzeczywista, 2);
+        let urojona = Math.pow(this.urojona, 2);
         return (Math.sqrt(rzeczywista + urojona));
-    };
-    Complex.prototype.toString = function () {
+    }
+    toString() {
         console.log('Rzeczywista: ', this.rzeczywista, 'Urojona: ', this.urojona);
-    };
-    return Complex;
-}());
-var x = new Complex(7, 3);
-var y = new Complex(6, 2);
-var z = x.dodawanie(y);
+    }
+}
+let x = new Complex(7, 3);
+let y = new Complex(6, 2);
+let z = x.dodawanie(y);
 z.toString();
-var tmp = z.odejmowanie(x);
+let tmp = z.odejmowanie(x);
 tmp.toString();
 console.log(tmp.modul());
