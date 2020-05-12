5159078bebe8822e068f7b265fa18410f904ffb2
ziolokeven <ziolokeven@gmail.com>
2020-05-12 11:52:28 +0200

    fix lab9

--- 3/blog/api/REST/post.endpoint.js
+++ 3/blog/api/REST/post.endpoint.js
@@ -1,13 +1,33 @@
 'use strict';
 import business from '../business/business.container';
+import applicationException from '../service/applicationException';
+
 const postEndpoint = (router) => {
-  router.get('/api/posts', async (request, response, next)=> {
+  router.get('/api/posts', async (request, response, next) => {
     try {
       let result = await
-        business(request).getCategoryManager().query();
+        business(request).getPostManager().query();
       response.status(200).send(result);
     } catch (error) {
-      console.log(error);
+      applicationException.errorHandler(error, response);
+    }
+  });
+
+  router.get('/api/posts/:id', async (request, response, next) => {
+    try {
+      let result = await business(request).getPostManager().get(request.params.id);
+      response.status(200).send(result);
+    } catch (error) {
+      applicationException.errorHandler(error, response);
+    }
+  });
+
+  router.post('/api/posts', async (request, response, next) => {
+    try {
+      let result = await business(request).getPostManager().createNewOrUpdate(request.body);
+      console.log(result);
+    } catch (error) {
+      applicationException.errorHandler(error, response);
     }
   });
 };
